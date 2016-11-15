44a5c4cf1106639915e451fbefdc1d33ec1d6914
黒羽快斗 <bh2rfq@qq.com>
2016-11-16 01:17:27 +0800

    make avr-gcc without avrlibc

--- avr-gcc/build.sh
+++ avr-gcc/build.sh
@@ -10,7 +10,9 @@ if [ "$(whoami)" != "root" ]; then
 	exit 1
 fi
 
+prefix=/opt/local
 binutils=binutils-2.27
+gcc=gcc-6.2.0
 
 echo "checking binutils"
 if [[ -x "$(command -v avr-ld)" ]]; then
@@ -42,8 +44,42 @@ else
 	fi
 	mkdir build
 	cd build
-	../configure --target=avr --prefix=/opt/local
+	../configure --target=avr --prefix=${prefix}
 	make
 	make install
 fi
 
+echo "checking gcc"
+if [[ -x "$(command -v avr-gcc)" ]]; then
+	echo "avr-gcc already exist."
+	echo "Please manual uninstall it if you want to compile it."
+	sleep 3
+else
+		#Get binutils
+	echo "Checking directory ${gcc}"
+	if [[ ! -d ./${gcc} ]]; then
+		echo "Directory binutils does not exist."
+		echo "Checking ${gcc}.tar.gz"
+		if [[ ! -f ./${gcc}.tar.gz ]]; then
+			echo "File ${gcc}.tar.gz does not exist."
+			echo "Try to get ${gcc}.tar.gz from ftp://ftp.gnu.org"
+			ftp ftp://ftp.gnu.org/gnu/gcc/${gcc}/${gcc}.tar.gz
+		fi
+		echo "unzip ${gcc}.tar.gz"
+		tar -xf ${gcc}.tar.gz
+	else
+		echo "Directory gcc does not exist."
+		echo "Please remove it if you want to redownload it."
+	fi
+
+	#Compile gcc.
+	cd ${gcc}
+	if [[ -d ./build ]]; then
+		rm -rf ./build
+	fi
+	mkdir build
+	cd build
+	../configure --target=avr --prefix=${prefix} --enable-fixed-point --enable-languages=c,c++ --enable-long-long --disable-nls --disable-werror
+	make all-gcc
+	make install
+fi
